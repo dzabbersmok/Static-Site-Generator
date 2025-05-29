feddf9e850145cdbc5ca9949bc21658a484ff6cb
ka3vas <tyrnis@gmail.com>
2025-05-29 14:19:34 +0200

    CH2:L4

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from leafnode import LeafNode
+# from textnode import TextNode, TextType
+# from htmlnode import HTMLNode
 
 def main():
-    test_text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    test_html_node = HTMLNode("p", "Some text", {"a": "test"}, {"href": "www.none.com"})
+    # test_text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    # test_html_node = HTMLNode("p", "Some text", {"a": "test"}, {"href": "www.none.com"})
+    test_leaf_node = LeafNode("p", "This is a paragraph of text.").to_html()
+    test_leaf_node2= LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
 
-    print(test_text_node)
-    print(test_html_node)
+    print(test_leaf_node)
+    print(test_leaf_node2)
 
 main()
---
+++ src/test_leafnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_eq(self):
+        node = repr(LeafNode("p", "This is a html p node"))
+        node2 = repr(LeafNode("p", "This is a html p node"))
+        self.assertEqual(node, node2)
+
+    def test_eq_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
+        self.assertEqual(node, "<a href=\"https://www.google.com\">Click me!</a>")
+
+    def test_eq_none_tag(self):
+        node = LeafNode(None, "Click me!")
+        print(node)
+
+if __name__ == "__main__":
+    unittest.main()
